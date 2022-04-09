c426f79d28fe426c3f120f0196700ecff010a601
buildaborg <mckeown.steve@gmail.com>
2022-04-08 21:57:53 -0400

    Create citiesstates.py

---
+++ citiesstates.py
@@ -0,0 +1,11 @@
+#mapping provinces to abbreviations
+provinces = {
+    'Ontario': 'ON',
+    'Quebec': 'QC',
+    'Manitoba': 'MB'
+    }
+
+cities = {
+    'ON': 'Toronto',
+    'QB': 'Montreal',
+    'MB': 'Winnipeg'}
