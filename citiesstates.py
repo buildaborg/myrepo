412d8faba5a3a92a638e5507f0e2b17785256bfb
buildaborg <mckeown.steve@gmail.com>
2022-04-25 21:27:37 -0400

    Update citiesstates.py

--- citiesstates.py
+++ citiesstates.py
@@ -2,10 +2,29 @@
 provinces = {
     'Ontario': 'ON',
     'Quebec': 'QC',
-    'Manitoba': 'MB'
+    'Manitoba': 'MB',
+    'British Columbia': 'BC',
+    'Alberta': 'AB'
     }
 
 cities = {
     'ON': 'Toronto',
     'QB': 'Montreal',
     'MB': 'Winnipeg'}
+
+cities['BC'] = 'Victoria'
+cities['AB'] = 'Calgary'
+
+print('-' * 10)
+print(cities)
+
+print('-' * 10)
+print("ON province has:", cities['ON'])
+print("BC Province has:", cities['BC'])
+
+print('-' * 10)
+print("Ontario's Abbreviation is:", provinces['Ontario'])
+print("Quebec's Abbreviation is:", provinces['Quebec'])
+
+print('-' * 10)
+print("Ontario has:", cities[provinces['Ontario']])
