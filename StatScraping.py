5d3b52bf61104fb1ea34d1e91e7c10cb43d90a50
buildaborg <mckeown.steve@gmail.com>
2023-01-26 21:47:22 -0500

    Update StatScraping.py

--- StatScraping.py
+++ StatScraping.py
@@ -4,15 +4,15 @@ Created on Fri Jan 13 20:56:19 2023
 
 @author: mckeo
 """
-
-import urllib.request, csv
+from bs4 import BeautifulSoup
+import urllib.request, csv, re
 
 from pprint import pprint
 
 from html_table_parser.parser import HTMLTableParser
 
 import pandas as pd
-Link = 'https://www.hockey-reference.com/players/m/mcdavco01.html'
+Link = 'https://www.hockey-reference.com/leagues/NHL_2023_skaters.html'
 
 #opens a website and reads binary response (HTML contents)
 def url_get_contents(targeturl):
@@ -25,6 +25,8 @@ def url_get_contents(targeturl):
 
 #DEFINE HTML CONTENTS OF A URL    
 xhtml = url_get_contents(Link).decode('utf-8')
+
+
 #defining the HTMLTableParser object
 p = HTMLTableParser()
 #feeding the html content in the HTMLTableParser object
@@ -33,15 +35,28 @@ p.feed(xhtml)
 
 #pprint(p.tables[0])
 print('\n\nPANDAS DATAFRAME\n')
-print(pd.DataFrame(p.tables[6]))
+print(pd.DataFrame(p.tables[0]))
 
 tablename = pd.read_html(Link)
-pprint(tablename[1])
+pprint(tablename[0])
 
 outputFile = 'D:/ScrapingTest.csv'
 CSV = open(outputFile, 'w', newline = '')
 
 writer = csv.writer(CSV)
 
-writer.writerows(p.tables[6])
+writer.writerows(p.tables[0])
 CSV.close()
+
+#beautfil soup links
+
+req2 = urllib.request.Request(Link)
+html_page = urllib.request.urlopen(req2)
+soup = BeautifulSoup(html_page, 'lxml')
+
+links = []
+for link in soup.findAll('a'):
+    
+    links.append(link.get('href'))
+    
+print(links)
